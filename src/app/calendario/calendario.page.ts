ddaa6d5b83041d14fe288e81c5b5ebc2f5a65beb
MARCELO MAYRINK MENDES <1A226849062@fumeccad.local>
2022-05-12 10:42:33 -0300

    Arrumando data do calendário e mensagem de saudação

--- src/app/calendario/calendario.page.ts
+++ src/app/calendario/calendario.page.ts
@@ -1,4 +1,6 @@
+import { getLocaleDateFormat, getLocaleDateTimeFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-calendario',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarioPage implements OnInit {
 
+  public data: string;
+  public saudacao: string;
+
   constructor() { }
 
   ngOnInit() {
+    
+    this.data = moment(new Date).format();
+    let hora_atual = moment().format('LT');
+    if(hora_atual <= '12:00' && hora_atual > '00:00'){
+      this.saudacao = 'Bom dia!';
+    }else if(hora_atual > '12:00' && hora_atual < '18:00'){
+      this.saudacao = 'Boa tarde!';
+    }else{
+      this.saudacao = 'Boa noite!';
+    }
+    
   }
 
 }
